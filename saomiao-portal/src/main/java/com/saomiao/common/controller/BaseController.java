903460285fe2b4e5619e1a6ee2cac4d5567bb212
jiazhifang000 <jzf18235182968@163.com>
2019-05-07 19:43:53 +0800

    //

--- saomiao-portal/src/main/java/com/saomiao/common/controller/BaseController.java
+++ saomiao-portal/src/main/java/com/saomiao/common/controller/BaseController.java
@@ -3,22 +3,23 @@ package com.saomiao.common.controller;
 import org.springframework.stereotype.Controller;
 
 import com.saomiao.common.utils.ShiroUtils;
+import com.saomiao.owneruser.domain.ManagersDO;
 import com.saomiao.owneruser.domain.OwnerUserDO;
 
 @Controller
 public class BaseController {
-	public OwnerUserDO getUser() {
+	public ManagersDO getUser() {
 		return ShiroUtils.getUser();
 	}
 
 	public Long getUserId() {
-		return getUser().getId();
+		return getUser().getMid();
 	}
 
 	public String getUsername() {
 		return getUser().getUsername();
 	}
 	public Long getforIds() {
-		return getUser().getUserId();
+		return getUser().getMid();
 	}
 }
